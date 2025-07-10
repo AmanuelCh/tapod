96c6c2f17da741b55f3e417ef189236541218b21
Amanuel Chaka <amanuelchaka2@gmail.com>
2025-07-10 23:46:14 +0300

    first commit

---
+++ wxt.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  srcDir: 'src',
+  modules: ['@wxt-dev/module-svelte'],
+  binaries: {
+    chrome: '/path/to/chrome-beta', // Use Chrome Beta instead of regular Chrome
+    firefox: 'firefoxdeveloperedition', // Use Firefox Developer Edition instead of regular Firefox
+    edge: '/path/to/edge', // Open MS Edge when running "wxt -b edge"
+  },
+  dev: {
+    browser: 'firefox', // 'chromium', 'firefox', or 'webkit'
+  },
+});
